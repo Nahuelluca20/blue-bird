360320f1320a956949ed5db1f3b6101deed072c7
Nahuel Luca <nahuelluca20@gmail.com>
2023-08-05 23:06:43 -0300

    add: Dinamicly render to button for login and logout

---
+++ app/auth-button-client.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {
+  createClientComponentClient,
+  Session,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  session: Session | null;
+}
+
+const AuthButtonClient: React.FC<Props> = ({session}) => {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return session ? (
+    <button onClick={handleSignOut}>Logout</button>
+  ) : (
+    <button onClick={handleSignIn}>Login</button>
+  );
+};
+
+export default AuthButtonClient;
