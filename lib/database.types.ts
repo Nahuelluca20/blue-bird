fc9fb5d71f76fc4d7f6c8f635260fb17b9148928
Nahuel Luca <nahuelluca20@gmail.com>
2023-08-06 16:54:58 -0300

    add: relational user_id with tweets

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -4,41 +4,51 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       tweets: {
         Row: {
-          created_at: string | null;
-          id: string;
-          title: string | null;
-        };
+          created_at: string | null
+          id: string
+          title: string | null
+          user_id: string
+        }
         Insert: {
-          created_at?: string | null;
-          id?: string;
-          title?: string | null;
-        };
+          created_at?: string | null
+          id?: string
+          title?: string | null
+          user_id: string
+        }
         Update: {
-          created_at?: string | null;
-          id?: string;
-          title?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string | null
+          id?: string
+          title?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "tweets_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
